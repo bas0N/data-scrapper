abcc49658c1f796eec86b8ea00615a4402876213
basON <wch.basinski@gmail.com>
2024-01-20 23:28:12 +0100

    Add minor changee

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-const main = async () => {
+const cron = require('node-cron');
 
+const main = async () => {
+  // Schedule a cron job to run every 15 seconds
+  cron.schedule('*/15 * * * * *', () => {
+    console.log('Cron job executed at:', new Date().toLocaleString());
+  });
 };
 
 main();
